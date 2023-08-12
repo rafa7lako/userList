70a92595156b036d91f1a452e1a58c4d7833f93b
rafa7lako <103770511+rafa7lako@users.noreply.github.com>
2023-08-12 22:34:01 +0200

    css stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<div className="main-window">
+				<label htmlFor="username">Username</label>
+				<input type="text" id="username" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<label htmlFor="age">Age (Years)</label>
+				<input type="number" id="age" />
+
+				<button className="addUserBtn">Add user</button>
+			</div>
+			<div className="userList">
+				<div className="userListItem">Max (31 years old)</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
