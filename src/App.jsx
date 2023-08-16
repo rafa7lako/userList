47390709ee93de8a84e608aaede165482b3792e2
rafa7lako <103770511+rafa7lako@users.noreply.github.com>
2023-08-16 19:21:22 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,41 @@ import React, { useState } from "react";
 
 import "./App.css";
 import MainWindow from "./components/MainWindow/MainWindow";
-import UserListList from "./components/userListList/userListList";
+import UserListList from "./components/UserListList/UserListList";
 import ErrorModal from "./components/ErrorModal/ErrorModal";
 
 const userArray = [];
 
 function App() {
-	
 	const [userListArray, setUserListArray] = useState(userArray);
-	const [ifOutputModal, setIfOutputModal] = useState(false)
+	const [ifOutputModal, setIfOutputModal] = useState(false);
 
-	
-	
-
-	const submitedData = (inputData) =>{
-		if(inputData.username != '' && inputData.age != ''){
+	const submitedData = (inputData) => {
+		if (
+			inputData.username.trim() != "" &&
+			inputData.age != "" &&
+			+inputData.age > 1
+		) {
+			setIfOutputModal(false);
 			setUserListArray((prevData) => {
-			return [inputData, ...prevData];
-		});
+				return [inputData, ...prevData];
+			});
 		} else {
-			 setIfOutputModal(true)
+			setIfOutputModal(true);
 		}
-		
-
 	};
 
-	
-
-	
+	const isBtnClicked = (clicked) => {
+		setIfOutputModal(clicked);
+	};
 
 	return (
 		<>
-			<MainWindow onSubmitedData={submitedData}/>
+			<MainWindow onSubmitedData={submitedData} />
 			{userListArray.length < 1 && <p>There are no users added.</p>}
-			{userListArray.length > 0 && <UserListList dataArray={userListArray}/>}
+			{userListArray.length > 0 && <UserListList dataArray={userListArray} />}
 
-			{ifOutputModal && <ErrorModal />}
+			{ifOutputModal ? <ErrorModal isBtnClicked={isBtnClicked} /> : ""}
 		</>
 	);
 }
--- src/components/ErrorModal/ErrorModal.jsx
+++ src/components/ErrorModal/ErrorModal.jsx
@@ -2,27 +2,29 @@ import React, { useState } from "react";
 
 import styles from "./ErrorModal.module.css";
 
-const ErrorModal = () => {
+const ErrorModal = (props) => {
 	const [btnClicked, isBtnClicked] = useState(false);
 
 	const onBtnClicked = () => {
 		if (btnClicked === false) {
 			isBtnClicked(true);
-		} else {
+			props.isBtnClicked(false)
+		} else if (btnClicked === true) {
 			isBtnClicked(false);
+			props.isBtnClicked(true)
 		}
 	};
 
 	return (
 		<>
-			<div className={`${styles["error-modal"]} ${btnClicked && styles.hidden}`}>
+			<div className={`${styles["error-modal"]} `}>
 				<div className={styles["invalid-input-row"]}>
 					<h2>Invalid input</h2>
 				</div>
 				<p>Please enter a valid name and age (non-empty values)</p>
 				<button onClick={onBtnClicked} className="modal-close">OK</button>
 			</div>
-			<div  className={`${styles.shadow} ${btnClicked && styles.hidden}`}></div>
+			<div onClick={onBtnClicked} className={`${styles.shadow}`}></div>
 		</>
 	);
 };
